cc3dc07f31a785732d454fc141ac864db6b136e3
Arshverma001 <87807771+Arshverma001@users.noreply.github.com>
2022-12-30 02:39:27 +0530

    second commit

--- src/Shop.js
+++ src/Shop.js
@@ -3,9 +3,9 @@ import React from "react";
 const Shop = () => {
   return (
     <div>
-      <h2>Buy ssd worth 1500</h2>
+      <h2>Deposite/Withdraw money</h2>
       <button className="btn btn-primary mx-2">-</button>
-      Add Item to cart
+      Update Balance
       <button className="btn btn-primary mx-2">+</button>
     </div>
   );
