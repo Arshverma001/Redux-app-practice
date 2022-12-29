3e1e702ec2001bd4b663a4da4fd54d5778e60f85
Arshverma001 <87807771+Arshverma001@users.noreply.github.com>
2022-12-29 23:01:18 +0530

    first commit

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <h2>Buy ssd worth 1500</h2>
+      <button className="btn btn-primary mx-2">-</button>
+      Add Item to cart
+      <button className="btn btn-primary mx-2">+</button>
+    </div>
+  );
+};
+
+export default Shop;
